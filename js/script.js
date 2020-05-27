edc5e549ba669df6a2a4812a0c70e31a566c32a1
fabioalberici <fabioalbe79@gmail.com>
2020-05-27 17:44:01 +0200

    esercizio ultimato

--- js/script.js
+++ js/script.js
@@ -2,16 +2,14 @@
 for (var i = 1; i <= 100; i++) {
   var numero = i;
   // nel caso sia un multiplo sia di 3 che di 5 scrivo 'fizzbuzz'
-  if ( i % 5 == 0 && i & 3 == 0) {
+  if ( i % 5 == 0 && i % 3 == 0) {
    numero = 'fizzbuzz';
-  }
-  // nel caso sia un numero multiplo di 5 scrivo 'buzz'
-  else if ( i % 5 == 0) {
+  } else if ( i % 5 == 0) {
+    // nel caso sia un numero multiplo di 5 scrivo 'buzz'
     numero = 'buzz';
-  }
-// nel caso sia un numero multiplo di 3 scrivo 'fizz'
-  else ( i % 3 == 0) {
+  } else if ( i % 3 == 0) {
+    // nel caso sia un numero multiplo di 3 scrivo 'fizz'
     numero = 'fizz';
   }
-  console.log(numero)
+  console.log(numero);
 }
